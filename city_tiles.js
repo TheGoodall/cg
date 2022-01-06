1a7ffc4495d1b5f8d929b0a2970131fa6af7ccf8
James Goodall <jatgoodall@protonmail.com>
2022-01-06 13:03:34 +0000

    added perlin noise

--- city_tiles.js
+++ city_tiles.js
@@ -1,7 +1,12 @@
 
-
-export function get_value(x, y, seed) {
-	
+perlin.seed();
+function perlin_noise(x, y, seed, scale) {
+	return perlin.get((x/scale)+(1000*seed), y/scale);
 }
+
+export function get_value(x, y) {
+	return perlin_noise(x, y, 0, 10);
+}
+
 
 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import * as THREE from './node_modules/three/src/Three.js';
 
 import * as TILES from './city_tiles.js';
 
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -10,20 +11,40 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 camera.position.z = 5;
 
+
+
+for (let i = 0; i < 15; i++) {
+	let line = "";
+	for (let j = 0; j < 15; j++) {
+		line += TILES.get_value(i, j);
+		line += ", "
+	}
+	console.log(line);
+}
+
+scene.fog = new THREE.FogExp2( 0xaaccff, 0.0007 );
+
+let geometry = new THREE.PlaneGeometry( 20000, 20000, 128, 128 );
+const position = geometry.attributes.position;
+position.usage = THREE.DynamicDrawUsage;
+for ( let i = 0; i < position.count; i ++ ) {
+	const y = 35* Math.sin( i/2 );
+	position.setY(i, y)
+}
+
+
+let material = new THREE.MeshStandardMaterial( { color: 0x0044ff } );
+
+let mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
 function animate() {
 	requestAnimationFrame( animate );
-	TILES.get_value(0, 0, 0);
 	
 	renderer.render( scene, camera );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 }
 animate();
