d5499a6aa8effc4b90285a3250e03d3e15aae4e4
James Goodall <jatgoodall@protonmail.com>
2022-01-20 15:16:53 +0000

    work

--- city_tiles.js
+++ city_tiles.js
@@ -1,11 +1,11 @@
 
 perlin.seed();
-function perlin_noise(x, y, seed, scale) {
-	return perlin.get((x/scale)+(1000*seed), y/scale);
+export function perlin_noise(x, y, seed, scale) {
+	return perlin.get((x/scale)+(1000*seed), y/scale+(1000*seed));
 }
 
 export function get_value(x, y) {
-	return perlin_noise(x, y, 0, 10);
+	return perlin_noise(x, y, 0, 100);
 }
 
 
